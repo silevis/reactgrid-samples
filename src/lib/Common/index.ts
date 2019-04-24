a6afb683b4a4bf3208c57b53e8e0bc527977c588
Michael Matejko <matejkml@gmail.com>
2019-04-24 17:48:32 +0200

    further cleanup

---
+++ src/lib/Common/index.ts
@@ -0,0 +1,7 @@
+export * from "./Behavior";
+export * from "./CellMatrix";
+export * from "./Constants";
+export * from "./GridContext";
+export * from "./GridController";
+export * from "./Model";
+export * from "./Range";
