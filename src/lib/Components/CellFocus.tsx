8ee81f007f0d4ed481ef3c5eb4ffbf5f3db67b44
Michael Matejko <matejkml@gmail.com>
2019-06-30 17:18:32 +0200

    improved drawing borders

--- src/lib/Components/CellFocus.tsx
+++ src/lib/Components/CellFocus.tsx
@@ -13,8 +13,8 @@ export const CellFocus: React.FunctionComponent<CellFocusProps> = (props) =>
             position: 'absolute',
             top: props.location.row.top - (props.location.row.top === 0 ? 0 : 1),
             left: props.location.col.left - (props.location.col.left === 0 ? 0 : 1),
-            width: props.location.col.width + (props.location.row.top === 0 ? 0 : 1),
-            height: props.location.row.height + (props.location.col.left === 0 ? 0 : 1),
+            width: props.location.col.width + (props.location.col.left === 0 ? 0 : 1),
+            height: props.location.row.height + (props.location.row.top === 0 ? 0 : 1),
             border: 'solid 2px #3579f8',
             pointerEvents: 'none' // prevent delegating events around cell
         }}
