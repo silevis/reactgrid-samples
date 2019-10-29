402cb946edc1d1da0b375154d8794fcc230bac1b
Michał Durak <michal.durak@silevis.com>
2019-10-29 11:02:27 +0100

    simplify the logic behind column and row reordering demo

--- src/samples/columnReorderSample/ColumnReorderSample.tsx
+++ src/samples/columnReorderSample/ColumnReorderSample.tsx
@@ -18,26 +18,27 @@ const DynaGridContainer = styled.div`
 export class ColumnReorderSample extends React.Component<{}, CellMatrixProps> {
 
   state = {
-    columns:  columns(true, false),
-    rows:     rows(true)
+    columns: columns(true, false),
+    rows: rows(true)
   }
 
   private getMatrix = (): CellMatrixProps => {
     const columns: ColumnProps[] = [...this.state.columns].map((c: ColumnProps, cIdx: number) => ({
       ...c,
       onDrop: (idxs: Id[]) => {
-        this.setState({ 
-          columns: this.getReorderedColumns(idxs as string[], cIdx),
-          rows: this.getUpdatedRows([...idxs].reverse().map((id: Id) => this.state.columns.findIndex(c => c.id === id)), cIdx) 
+        const arrayIdxs = idxs.map((id: Id) => this.state.columns.findIndex(c => c.id === id));
+        this.setState({
+          columns: this.reorderArray(this.state.columns, arrayIdxs, cIdx),
+          rows: this.state.rows.map(row => ({ ...row, cells: this.reorderArray(row.cells, arrayIdxs, cIdx) })),
         });
       }
     }))
     const rows: RowProps[] = [...this.state.rows].map((r: RowProps, rIdx: number) => ({
       ...r,
-      onDrop: (idxs: Id[]) => this.setState({ 
-        rows: this.getReorderedRows(idxs as string[], rIdx) 
+      onDrop: (idxs: Id[]) => this.setState({
+        rows: this.reorderArray(this.state.rows, idxs.map((id: Id) => this.state.rows.findIndex(c => c.id === id)), rIdx),
       })
-     }))
+    }))
     return { rows, columns }
   }
 
@@ -45,48 +46,23 @@ export class ColumnReorderSample extends React.Component<{}, CellMatrixProps> {
     const state = { ...this.state };
     dataChanges.forEach((change: DataChange) => {
       const columnIndex: number = this.state.columns.findIndex((column: ColumnProps) => column.id === change.columnId)
-      state.rows.forEach((r: RowProps) => { r.id == change.rowId ? r.cells[columnIndex].data = change.newData : r})
+      state.rows.forEach((r: RowProps) => { r.id == change.rowId ? r.cells[columnIndex].data = change.newData : r })
     })
     return state
   }
 
-  private getReorderedColumns(colIds: Id[], to: number) {
-    const movedColumns: ColumnProps[] = [...this.state.columns].filter((c: ColumnProps) => colIds.includes(c.id));
-    const clearedColumns: ColumnProps[] = [...this.state.columns].filter((c: ColumnProps) => !colIds.includes(c.id));
-    if (to > [...this.state.columns].findIndex((c: ColumnProps) => c.id == colIds[0]))
-      to -= colIds.length - 1
-    clearedColumns.splice(to, 0, ...movedColumns)
-    return clearedColumns
-  }
+  private reorderArray(arr: any[], idxs: number[], to: number) {
+    const movedElements: any[] = arr.filter((e, idx) => idxs.includes(idx));
 
-  private getUpdatedRows(from: number[], to: number): RowProps[] {
-    let newRows: RowProps[] = [];
-    this.state.rows.forEach((row: RowProps) => {
-      const newRow: RowProps = { ...row };
-      const newCells: Cell[] = [...row.cells];
-      let i = 0;
-      from.forEach((fromIdx: number) => {
-        if (to > fromIdx) {
-          newCells.splice(to - i, 0, newCells.splice(fromIdx, 1)[0]);
-          i++;
-        } else {
-          newCells.splice(to, 0, newCells.splice(fromIdx - i, 1)[0]);
-          i--;
-        }
-      })
-      newRow.cells = newCells;
-      newRows = [...newRows, newRow];
-    })
-    return newRows;
-  }
+    if (Math.min(...idxs) < to)
+      to += 1;
+    else
+      to -= idxs.filter(idx => idx < to).length;
 
-  private getReorderedRows(rowIds: Id[], to: number): RowProps[] {
-    const movedRows = [...this.state.rows].filter(r => rowIds.includes(r.id));
-    const clearedRows = [...this.state.rows].filter(r => !rowIds.includes(r.id));
-    if (to > [...this.state.rows].findIndex(r => r.id == rowIds[0]))
-      to -= rowIds.length - 1
-    clearedRows.splice(to, 0, ...movedRows)
-    return clearedRows
+    const leftSide: any[] = arr.filter((e, idx) => idx < to && !idxs.includes(idx));
+    const rightSide: any[] = arr.filter((e, idx) => idx >= to && !idxs.includes(idx));
+
+    return [...leftSide, ...movedElements, ...rightSide];
   }
 
   render() {
