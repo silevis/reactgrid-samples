e3b9fde53337247bd670820c659cedfc764e1069
krzysztof.sawicki <krzysztof.sawicki@silevis.com>
2019-10-31 15:58:31 +0100

    add

--- src/samples/stockMarketDataSample/StockMarketDataSample.tsx
+++ src/samples/stockMarketDataSample/StockMarketDataSample.tsx
@@ -9,6 +9,7 @@ const ReactGridContainer = styled.div`
   position: relative;
   margin-left: 10px;
   width: 100%;
+  height: 1200px;
   min-height: 400px;
 `
 
@@ -52,7 +53,7 @@ const fields: ColumnProps[] = [{
 ]
 
 const api = async () => {
-  //console.log('api');
+
   const data = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd");
   const myJsonData = await data.json();
   const newRows: RowProps[] = [{
@@ -76,7 +77,7 @@ const api = async () => {
       cells: [
         { type: 'text', data: item.name },
         { type: 'text', data: item.symbol },
-        { type: 'number', data: item.current_price },
+        { type: 'number', data: item.current_price, props: { backgroundColor: 'none' } },
         { type: 'number', data: item.low_24h },
         { type: 'number', data: item.high_24h },
         { type: 'number', data: item.high_24h }
@@ -106,62 +107,48 @@ export default class StockMarketDataSample extends React.Component {
 
   intervalId?: number;
 
-
-
-  private async findRow() {
-    console.log('findrow');
-    const newRows: RowProps[] = await api();
-    const statRows: RowProps[] = [...this.state.rows]
-
-
-
-
-    let callback = newRows.map((item: RowProps, idx) => {
-
-      if (statRows.length > 0 && item.cells[2].data !== statRows[idx].cells[2].data) {
-
-        console.log(item.cells[2], statRows[idx].cells[2])
-        // console.log(item)
-        // console.log('test', item.cells[2], statRows[idx].cells[2])
-        return {
-          id: item.id,
-          reorderable: false,
-          height: 25,
-          cells: [...item.cells, (item.cells[2].type = "styleInside")]
-        };
-
-      }
-      else {
-        return {
-          id: item.id,
-          reorderable: false,
-          height: 25,
-          cells: [...item.cells]
-        };
-      }
-    });
-    // this.setState(
-    //   {
-    //     rows: callback
-    //   })
-
-  }
-
-
   componentDidMount() {
     const renderValue = async () => {
-      const newRows: RowProps[] = await api();
-      this.setState(
-        {
-          rows: newRows
-        }
-      )
-    }
-    renderValue();
+      const dataApi: RowProps[] = await api();
+      const dataState: RowProps[] = [...this.state.rows]
 
+      const changedIdx = Math.floor(Math.random() * 10 + 1);
+
+
+      const returnRandom = () => {
+        let randomValue = 0
+        dataState.forEach((item, idx) => {
+          if (idx == changedIdx) {
+            const min = item.cells[3].data;
+            const max = item.cells[4].data;
+            randomValue = (Math.random() * (+max - +min) + +min);
+            Number(randomValue).toFixed(8);
+          }
+        })
+        return randomValue
+      }
+
+      dataState.forEach((data, ids) => {
+        dataApi.forEach((api, idt) => {
+          if (returnRandom() !== dataState[changedIdx].cells[2].data) {
+            dataApi[changedIdx].cells[2].data = returnRandom()
+          }
+          if (dataState.length > 0 && api.cells[2].data !== data.cells[2].data && ids == idt) {
+
+            api.cells[2].props.backgroundColor = 'red'
+            // api.cells[2].type = 'styleInside'
+          }
+
+
+        })
+      })
+      this.setState({ rows: dataApi })
+    }
+
+    renderValue();
     this.intervalId = setInterval(
       () => { renderValue() }
-      , 1000);
+      , 3000);
   }
 
 
@@ -173,9 +160,7 @@ export default class StockMarketDataSample extends React.Component {
   // zapisuje dane, poprzednie przenosze do buuforra czyli tablicy, wyciagnac z aktulnej tablicy (state) liste wysztkich krypto walut ( (nazwa i id) jej nr w kolejnosci ), porownac obecny stan z poprzednim wyciagajac idetyfiaktory wierszy,
   // mamy liste wierszy ktora ulegay zmieninie, dla tych wierszy zminic typ komory na styleInside, 
 
-
   render() {
-    { this.findRow() }
     return (
       <>
         <ReactGridContainer className="resize-cell-sample">
