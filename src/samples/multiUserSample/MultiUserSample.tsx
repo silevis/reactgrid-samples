8910c93e40bbbc97a3e09a39d42b219096be3ab9
Patryk Eliasz <patryk0493@gmail.com>
2019-11-08 12:30:00 +0100

    added column resize and reorder to multi usr sampl

--- src/samples/multiUserSample/MultiUserSample.tsx
+++ src/samples/multiUserSample/MultiUserSample.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ReactGrid, DataChange, Focus, CellMatrixProps } from '@silevis/reactgrid';
+import { ReactGrid, DataChange, Focus, CellMatrixProps, RowProps, ColumnProps, Id } from '@silevis/reactgrid';
 import { RateCellTemplate } from '../../cell-templates/rateCell/RateCellTemplate';
 import { FlagCellTemplate } from '../../cell-templates/flagCell/FlagCellTemplate';
 import { DropdownNumberCellTemplate } from '../../cell-templates/dropdownNumberCell/DropdownNumberCellTemplate';
@@ -21,9 +21,9 @@ export interface IMultiUserSampleState extends CellMatrixProps {
 export class MultiUserSample extends React.Component<{}, IMultiUserSampleState> {
 
   state = {
-    columns: columns(false, false),
-    rows: rows(false),
-    focuses: []
+    columns:  columns(true, true),
+    rows:     rows(false),
+    focuses:  []
   }
 
   intervalId?: number;
@@ -65,24 +65,51 @@ export class MultiUserSample extends React.Component<{}, IMultiUserSampleState> 
   }
 
   private prepareDataChanges = (dataChanges: DataChange[]): IMultiUserSampleState => {
-    const state = { ...this.state }
-    dataChanges.forEach(change => {
-      state.rows.forEach((row: any) => {
-        if (row.id == change.rowId) {
-          const field = this.state.columns.findIndex((column: any) => column.id == change.columnId)
-          if (field !== undefined)
-            row.cells[field].data = change.newData;
-        }
-      })
+    const state = { ...this.state };
+    dataChanges.forEach((change: DataChange) => {
+      const columnIndex: number = this.state.columns.findIndex((column: ColumnProps) => column.id === change.columnId)
+      state.rows.forEach((row: RowProps) => { row.id == change.rowId ? row.cells[columnIndex].data = change.newData : row })
     })
     return state
+  }
+
+  private reorderArray(arr: any[], idxs: number[], to: number) {
+    const movedElements: any[] = arr.filter((_: any[], idx: number) => idxs.includes(idx));
+    to = Math.min(...idxs) < to ? to += 1 : to -= idxs.filter(idx => idx < to).length;
+    const leftSide: any[] = arr.filter((_: any, idx: number) => idx < to && !idxs.includes(idx));
+    const rightSide: any[] = arr.filter((_: any, idx: number) => idx >= to && !idxs.includes(idx));
+    return [...leftSide, ...movedElements, ...rightSide];
+  }
+
+  private getMatrix = (): CellMatrixProps => {
+    const columns: ColumnProps[] = [...this.state.columns].map((column: ColumnProps, cIdx: number) => ({
+      ...column,
+      onDrop: (idxs: Id[]) => {
+        const arrayIndexes = idxs.map((id: Id) => this.state.columns.findIndex((column: ColumnProps) => column.id === id));
+        this.setState({
+          columns: this.reorderArray(this.state.columns, arrayIndexes, cIdx),
+          rows: this.state.rows.map(row => ({ ...row, cells: this.reorderArray(row.cells, arrayIndexes, cIdx) })),
+        });
+      },
+      onResize: width => {
+        columns[cIdx] = { ...column, width };
+        this.setState({ columns })
+      }
+    }))
+    const rows: RowProps[] = [...this.state.rows].map((row: RowProps, rIdx: number) => ({
+      ...row,
+      onDrop: (idxs: Id[]) => this.setState({
+        rows: this.reorderArray(this.state.rows, idxs.map((id: Id) => this.state.rows.findIndex(r => r.id === id)), rIdx)
+      })
+    }))
+    return { rows, columns }
   }
 
   render() {
     return (
       <ReactGridContainer id="multi-user-sample">
         <ReactGrid
-          cellMatrixProps={this.state}
+          cellMatrixProps={this.getMatrix()}
           cellTemplates={{
             'rating': new RateCellTemplate,
             'flag': new FlagCellTemplate,
@@ -91,7 +118,6 @@ export class MultiUserSample extends React.Component<{}, IMultiUserSampleState> 
           customFocuses={this.state.focuses}
           onDataChanged={changes => this.setState(this.prepareDataChanges(changes))}
           license={'non-commercial'}
-          disableColumnSelection
         />
       </ReactGridContainer>
     )
