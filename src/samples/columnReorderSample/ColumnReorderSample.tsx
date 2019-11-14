92a757ae29b9f65078b7b2fe455cfe7e6d723c09
Patryk Eliasz <patryk0493@gmail.com>
2019-11-14 13:10:40 +0100

    rows reorder

--- src/samples/columnReorderSample/ColumnReorderSample.tsx
+++ src/samples/columnReorderSample/ColumnReorderSample.tsx
@@ -19,46 +19,10 @@ interface ColumnReorderGridState {
 
 export const ColumnReorderSample: React.FunctionComponent = () => {
 
-  const [state, setState] = React.useState<ColumnReorderGridState>(() => {
-    const columns: Column[] = (dataColumns(true, false) as Column[]).map((column: Column, cIdx: number) => ({
-      ...column,
-      columnId: column.columnId,
-      // onDrop: (idxs: Id[]) => {
-      //   const arrayIndexes = idxs.map((id: Id) => state.columns.findIndex((column: Column) => column.columnId === id));
-      //   setState({
-      //     columns: reorderArray(state.columns, arrayIndexes, cIdx),
-      //     rows: state.rows.map(row => ({ ...row, cells: reorderArray(row.cells, arrayIndexes, cIdx) })),
-      //   });
-      // }
-    }))
-    const rows: Row[] = (dataRows(true) as Row[]).map((row: Row, rIdx: number) => ({
-      ...row,
-      // onDrop: (idxs: Id[]) => setState({
-      //   rows: reorderArray(state.rows, idxs.map((id: Id) => state.rows.findIndex(r => r.rowId === id)), rIdx)
-      // })
-    }))
-    return { rows, columns }
-  })
-
-  // private getMatrix = (): ColumnReorderGridState => {
-  //   const columns: Column[] = [...this.state.columns].map((column: Column, cIdx: number) => ({
-  //     ...column,
-  //     onDrop: (idxs: Id[]) => {
-  //       const arrayIndexes = idxs.map((id: Id) => this.state.columns.findIndex((column: Column) => column.columnId === id));
-  //       this.setState({
-  //         columns: this.reorderArray(this.state.columns, arrayIndexes, cIdx),
-  //         rows: this.state.rows.map(row => ({ ...row, cells: this.reorderArray(row.cells, arrayIndexes, cIdx) })),
-  //       });
-  //     }
-  //   }))
-  //   const rows: Row[] = [...this.state.rows].map((row: Row, rIdx: number) => ({
-  //     ...row,
-  //     onDrop: (idxs: Id[]) => this.setState({
-  //       rows: this.reorderArray(this.state.rows, idxs.map((id: Id) => this.state.rows.findIndex(r => r.id === id)), rIdx)
-  //     })
-  //   }))
-  //   return { rows, columns }
-  // }
+  const [state, setState] = React.useState<ColumnReorderGridState>(() => ({ 
+      columns:  dataColumns(true, false),
+      rows:     dataRows(true), 
+  }))
 
   const handleChanges = (changes: CellChange[]) => {
     let newState = { ...state };
@@ -83,16 +47,25 @@ export const ColumnReorderSample: React.FunctionComponent = () => {
     return true;
   }
 
+  const handleCanReorderRows = (targetColumnId: Id, columnIds: Id[], dropPosition: DropPosition): boolean => {
+    return true;
+  }
+
   const handleColumnsReordered = (targetColumnId: Id, columnIds: Id[], dropPosition: DropPosition) => {
-    const arrayIndexes = columnIds.map((id: Id) => { 
-      return id as number
-    });
     const to = state.columns.findIndex((column: Column) => column.columnId === targetColumnId);
-    console.log(state.rows.map(row => ({ ...row, cells: reorderArray(row.cells, arrayIndexes as number[], targetColumnId as number) })));
-    
     setState({
-      rows: state.rows.map(row => ({ ...row, cells: reorderArray(row.cells, arrayIndexes as number[], targetColumnId as number) })),
-      columns: reorderArray(state.columns, arrayIndexes as number[], targetColumnId as number)
+      columns: reorderArray(state.columns, columnIds as number[], to),
+      rows: state.rows.map(row => ({ ...row, cells: reorderArray(row.cells, columnIds as number[], to) })),
+    });
+  }
+
+  const handleRowsReordered = (targetRowId: Id, rowIds: Id[], dropPosition: DropPosition) => {
+    let newState = { ...state };
+    const to = state.rows.findIndex((row: Row) => row.rowId === targetRowId);
+    const ids = rowIds.map((id: Id) => state.rows.findIndex(r => r.rowId === id)) as number[];
+    setState({
+      ...newState,
+      rows: reorderArray(state.rows, ids, to)
     });
   }
 
@@ -107,9 +80,12 @@ export const ColumnReorderSample: React.FunctionComponent = () => {
         }}
         onCellsChanged={handleChanges}
         license={'non-commercial'}
-        canReorderRows={handleCanReorderColumns}
+        canReorderColumns={handleCanReorderColumns}
+        canReorderRows={handleCanReorderRows}
         onColumnsReordered={handleColumnsReordered}
+        onRowsReordered={handleRowsReordered}
         enableColumnSelection
+        enableRowSelection
       />
     </ReactGridContainer>
   )
