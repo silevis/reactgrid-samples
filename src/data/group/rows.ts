94aa1eb82eeaa7629ca9aaca9cafb46bcf8a75d5
myskamil <kamil.myslek@silevis.com>
2019-12-06 12:59:07 +0100

    Group cell sample, not ended, one bug to repair

--- src/data/group/rows.ts
+++ src/data/group/rows.ts
@@ -1,28 +1,197 @@
-import { TextCell, NumberCell, GroupCell } from '@silevis/reactgrid';
+import { TextCell, NumberCell, GroupCell, HeaderCell } from '@silevis/reactgrid';
 
 export const rows = (reorderable: boolean): any[] => [
-  {
+ {
     rowId: 'header',
     height: 25,
     reorderable,
     cells: [
-      { type: 'header', text: 'Id' },
-      { type: 'header', text: 'Branch Name' },
-      { type: 'header', text: 'Commit Hash' },
-      { type: 'header', text: 'Added' },
-      { type: 'header', text: 'Removed' },
+      { type: 'header', text: 'Id' } as HeaderCell,
+      { type: 'header', text: 'Branch Name' } as HeaderCell,
+      { type: 'header', text: 'Commit Hash' } as HeaderCell,
+      { type: 'header', text: 'Added' } as HeaderCell,
+      { type: 'header', text: 'Removed' } as HeaderCell,
     ]
   },
   {
-    rowId: '1',
+    rowId: 1,
     height: 25,
     reorderable,
     cells: [
-      { type: 'group', text: 'is', depth: 0, isExpanded: true } as GroupCell,
+      { type: 'group', text: 'is', isExpanded: true, hasChildrens: true} as GroupCell,
       { type: 'text', text: 'fix/some-feature' } as TextCell,
       { type: 'text', text: 'e989109363ec42610966f85fe9b065e6017058f7' } as TextCell,
-      { type: 'number', value: 500 } as NumberCell,
+      { type: 'number', value: 890 } as NumberCell,
+      { type: 'number', value: 120 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 2,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 1 } as GroupCell,
+      { type: 'text', text: 'fix/some-feature' } as TextCell,
+      { type: 'text', text: 'ey5seefv1o8soch1q50ztl30bzhubtb1xg6oklup' } as TextCell,
+      { type: 'number', value: 310 } as NumberCell,
+      { type: 'number', value: 490 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 3,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, hasChildrens: true } as GroupCell,
+      { type: 'text', text: 'fix/my-sample' } as TextCell,
+      { type: 'text', text: 'u61x66unzgl9xd5gre3bj7g8za8cb7ve4t7otz0e' } as TextCell,
+      { type: 'number', value: 280 } as NumberCell,
+      { type: 'number', value: 120 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 4,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 3 } as GroupCell,
+      { type: 'text', text: 'fix/my-sample' } as TextCell,
+      { type: 'text', text: 'v2dwm51y0k874x596axt4uz1if5qcv7etavg76va' } as TextCell,
+      { type: 'number', value: 400 } as NumberCell,
+      { type: 'number', value: 350 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 5,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 3 } as GroupCell,
+      { type: 'text', text: 'fix/my-sample' } as TextCell,
+      { type: 'text', text: 'jqk6nn3wktt2nwituttafuvpv7hlzo2grelvs7vo' } as TextCell,
+      { type: 'number', value: 150 } as NumberCell,
+      { type: 'number', value: 900 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 6,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 3 } as GroupCell,
+      { type: 'text', text: 'fix/my-sample' } as TextCell,
+      { type: 'text', text: 'ppsqily4doxz27uw6tznvc3qfvfhc37500k59jw9' } as TextCell,
+      { type: 'number', value: 400 } as NumberCell,
+      { type: 'number', value: 200 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 7,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, hasChildrens: true } as GroupCell,
+      { type: 'text', text: 'fix/other-feature' } as TextCell,
+      { type: 'text', text: 'uc75daha01rnk3dfcghvkgav13igsb87b0w1jzft' } as TextCell,
+      { type: 'number', value: 600 } as NumberCell,
       { type: 'number', value: 500 } as NumberCell,
     ]
   },
+  {
+    rowId: 8,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 7 } as GroupCell,
+      { type: 'text', text: 'fix/other-feature' } as TextCell,
+      { type: 'text', text: 'bmwz5y30ypjgixzh3aic3vpjlnh1q1hrie2pv5mg' } as TextCell,
+      { type: 'number', value: 800 } as NumberCell,
+      { type: 'number', value: 120 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 9,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 7 } as GroupCell,
+      { type: 'text', text: 'fix/other-feature' } as TextCell,
+      { type: 'text', text: 'rc3hmvkwh4to6iq8mo68ju9vyx2zcmqbgn73zrw9' } as TextCell,
+      { type: 'number', value: 220 } as NumberCell,
+      { type: 'number', value: 110 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 10,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 7 } as GroupCell,
+      { type: 'text', text: 'fix/other-feature' } as TextCell,
+      { type: 'text', text: '1ooxkvmvwotxicvawyh0wb1ur8jtin12egyayee8' } as TextCell,
+      { type: 'number', value: 190 } as NumberCell,
+      { type: 'number', value: 70 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 11,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, hasChildrens: true } as GroupCell,
+      { type: 'text', text: 'update/some-feature' } as TextCell,
+      { type: 'text', text: 'fvgiizz61ysmiv2gn9por6izio575u557jyxz4xs' } as TextCell,
+      { type: 'number', value: 120 } as NumberCell,
+      { type: 'number', value: 70 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 12,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 11 } as GroupCell,
+      { type: 'text', text: 'update/some-feature' } as TextCell,
+      { type: 'text', text: 'rbicj7u5qxvkpqv2ti2bkthlw4yg1by4ht4c1wom' } as TextCell,
+      { type: 'number', value: 400 } as NumberCell,
+      { type: 'number', value: 250 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 13,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 11 } as GroupCell,
+      { type: 'text', text: 'update/some-feature' } as TextCell,
+      { type: 'text', text: 'cunj4bkbl2gow91atjtfcwko1zmqp6813l8x626v' } as TextCell,
+      { type: 'number', value: 15 } as NumberCell,
+      { type: 'number', value: 60 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 14,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 11 } as GroupCell,
+      { type: 'text', text: 'update/some-feature' } as TextCell,
+      { type: 'text', text: 'iwpnwef8mtzsjyu1srihdwispyrjxvb5197ey6cz' } as TextCell,
+      { type: 'number', value: 800 } as NumberCell,
+      { type: 'number', value: 300 } as NumberCell,
+    ]
+  },
+  {
+    rowId: 15,
+    height: 25,
+    reorderable,
+    cells: [
+      { type: 'group', text: 'is', depth: 0, isExpanded: true, parentId: 11 } as GroupCell,
+      { type: 'text', text: 'update/some-feature' } as TextCell,
+      { type: 'text', text: '6wawna3wf02eggw27v8kgyclhla2c82apmdemay4' } as TextCell,
+      { type: 'number', value: 200 } as NumberCell,
+      { type: 'number', value: 40 } as NumberCell,
+    ]
+  },
+
 ]
--- src/samples/groupCellSample/GroupCellSample.tsx
+++ src/samples/groupCellSample/GroupCellSample.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
-import { ReactGrid, CellChange, Column, Row } from '@silevis/reactgrid';
-import { RateCellTemplate } from '../../cell-templates/rateCell/RateCellTemplate';
-import { FlagCellTemplate } from '../../cell-templates/flagCell/FlagCellTemplate';
 import { columns as dataColumns } from '../../data/group/columns';
 import { rows as dataRows } from '../../data/group/rows';
+import {Cell, CellChange, Column, GroupCell, Id, ReactGrid, Row, TextCell, NumberCell, HeaderCell} from '@silevis/reactgrid'
 import './styling.scss';
 
 const ReactGridContainer = styled.div`
@@ -12,43 +10,163 @@ const ReactGridContainer = styled.div`
   min-height: 400px;
 `;
 
-interface ColumnReorderGridState {
-  columns:  Column[]
-  rows:     Row[]
+interface GroupTestGridStateData {
+    columns:    Column[]
+    rows:       Row[]
 }
 
 export const GroupCellSample: React.FunctionComponent = () => {
 
-  const [state, setState] = React.useState<ColumnReorderGridState>(() => ({
-    columns:  dataColumns(false, false),
-    rows:     dataRows(false),
-  }))
+    const getGroupCell = (row: Row): GroupCell => row.cells.find((cell: Cell) => cell.type === 'group') as GroupCell;
 
-  const handleChanges = (changes: CellChange[]) => {
-    let newState = { ...state };
-    changes.forEach((change: any) => {
-      const changeRowIdx = newState.rows.findIndex(el => el.rowId === change.rowId);
-      const changeColumnIdx = newState.columns.findIndex(el => el.columnId === change.columnId);
-      newState.rows[changeRowIdx].cells[changeColumnIdx] = change.newCell;
-    })
-    setState(newState);
-    return true;
-  }
+    const hasChildren = (rows: Row[], row: Row): boolean => rows.some((r: Row) => getGroupCell(r).parentId === row.rowId);
 
-  return (
+    const isRowFullyExpanded = (rows: Row[], row: Row): boolean => {
+      const parentRow = getParentRow(rows, row);
+      if (parentRow) {
+        if (!getGroupCell(parentRow).isExpanded) return false;
+        return isRowFullyExpanded(rows, parentRow);
+      }
+      return true
+    };
+
+    const getExpandedRows = (rows: Row[]): Row[] => {
+        return rows.filter((row: Row) => {
+            const areAllParentsExpanded = isRowFullyExpanded(rows, row);
+            return areAllParentsExpanded !== undefined ? areAllParentsExpanded : true;
+        });
+    };
+
+    const getDirectChildrenRows = (rows: Row[], parentRow: Row): Row[] => rows.filter((row: Row) => !!row.cells.find((cell: Cell) => cell.type === 'group' && (cell as GroupCell).parentId === parentRow.rowId));
+
+    const getParentRow = (rows: Row[], row: Row): Row | undefined => rows.find((r: Row) => r.rowId === getGroupCell(row).parentId); 
+
+    const assignIndentAndHasChildrens = (allRows: Row[], parentRow: Row, indent: number) => {
+        ++indent;
+        getDirectChildrenRows(allRows, parentRow).forEach((row: Row) => {
+            const groupCell = getGroupCell(row);
+            groupCell.indent = indent;
+            const hasRowChildrens = hasChildren(allRows, row);
+            groupCell.hasChildrens = hasRowChildrens;
+            if (hasRowChildrens) assignIndentAndHasChildrens(allRows, row, indent);
+        });
+    };
+
+    const createIndents = (rows: Row[]): Row[] => {
+      console.log('abcd');
+        return rows.map((row: Row) => {
+            const groupCell: GroupCell = getGroupCell(row);
+            
+            if (groupCell.parentId === undefined) {
+               console.log("włazi");
+                const hasRowChildrens = hasChildren(rows, row);
+                console.log(hasRowChildrens);
+                groupCell.hasChildrens = hasRowChildrens;
+                if (hasRowChildrens) assignIndentAndHasChildrens(rows, row, 0);
+            }
+            return row;
+        });
+    };
+
+    const getRowsFromData = (): Row[] => {
+      let dataList = dataRows(true);
+      dataList = dataList.slice(1,dataList.length);
+      console.log(dataList);
+       return [ ...dataList ].map((dataRow: any): Row => {
+          if(dataRow.rowId === 'header'){
+            return {
+              rowId: dataRow.rowId,
+                cells: [
+                    {
+                        type: 'group',
+                        text: `id`,
+                        parentId: dataRow.cells[0].parentId,
+                        isExpanded: true,
+                    } as GroupCell,
+                    { type: 'header', text: `${dataRow.cells[1].text}`} as HeaderCell,
+                    { type: 'header', text: `${dataRow.cells[2].text}`} as HeaderCell,
+                    { type: 'header', text: dataRow.cells[3].text } as HeaderCell,
+                    { type: 'header', text: dataRow.cells[4].text } as HeaderCell,
+                  ]
+            }
+          }
+          else{
+    
+            return {
+                rowId: dataRow.rowId,
+                cells: [
+                    {
+                        type: 'group',
+                        text: ` ${dataRow.rowId}`,
+                        parentId: dataRow.cells[0].parentId,
+                        isExpanded: true,
+                    } as GroupCell,
+                    { type: 'text', text: `${dataRow.cells[1].text}`} as TextCell,
+                    { type: 'text', text: `${dataRow.cells[2].text}`} as TextCell,
+                    { type: 'number', value: dataRow.cells[3].value } as NumberCell,
+                    { type: 'number', value: dataRow.cells[4].value } as NumberCell,
+                ]
+            }
+          }
+        }
+        );
+    };
+
+    const [state, setState] = useState<GroupTestGridStateData>(() => {
+        const columns: Column[] = dataColumns(true, false);
+        let rows: Row[] = getRowsFromData();
+        rows = createIndents(rows);
+        rows = getExpandedRows(rows);
+        return { columns, rows }
+    });
+   
+    const [rowsToRender, setRowsToRender] = useState<Row[]>([ ...state.rows ]);
+
+    const handleColumnResize = (ci: Id, width: number) => {
+        let newState = { ...state };
+        const columnIndex = newState.columns.findIndex(el => el.columnId === ci);
+        const resizedColumn: Column = newState.columns[columnIndex];
+        newState.columns[columnIndex] = { ...resizedColumn, width };
+        setState(newState);
+    };
+
+    const handleChanges = (changes: CellChange[]) => {
+        const newState = { ...state };
+        changes.forEach((change: CellChange) => {
+            const changeRowIdx = newState.rows.findIndex(el => el.rowId === change.rowId);
+            const changeColumnIdx = newState.columns.findIndex(el => el.columnId === change.columnId);
+            newState.rows[changeRowIdx].cells[changeColumnIdx] = change.newCell;
+        });
+
+        setState({ ...state, rows: createIndents(newState.rows) });
+        setRowsToRender([
+          {
+            rowId: -1,
+              cells: [
+                  { type: 'header', text: `Id`} as HeaderCell,
+                  { type: 'header', text: `Branch Name`} as HeaderCell,
+                  { type: 'header', text: `Commit Hash`} as HeaderCell,
+                  { type: 'header', text: `Added`} as HeaderCell,
+                  { type: 'header', text: `Removed` } as HeaderCell,
+                ]
+          },
+        
+          ...getExpandedRows(newState.rows)]);
+        return true;
+    };
+
+    return(
     <ReactGridContainer id="group-cell-sample">
-      <ReactGrid
-        rows={state.rows}
-        columns={state.columns}
-        customCellTemplates={{
-          'rating': new RateCellTemplate,
-          'flag': new FlagCellTemplate
-        }}
-        onCellsChanged={handleChanges}
-        license={'non-commercial'}
-        enableColumnSelection
-        enableRowSelection
-      />
-    </ReactGridContainer>
-  )
+            <ReactGrid
+          rows={rowsToRender}
+          columns={state.columns}
+          license={'non-commercial'}
+          onCellsChanged={handleChanges}
+          onColumnResized={handleColumnResize}
+          frozenLeftColumns={1}
+          enableRowSelection
+          enableColumnSelection
+    />
+    </ReactGridContainer>  
+    )
 }
