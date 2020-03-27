6c69f58a6e369db345ef2652b4e3ce1c5d3fc660
Patryk Eliasz <patryk0493@gmail.com>
2020-03-27 06:56:55 +0100

    packages update

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { NavigationSample } from './samples/navigationSample/NavigationSample';
 
-// TODO how to remove this?
-import "core-js/stable";
-
 // DO NOT MOVE
 // this index.tsx is required by react-scripts-ts
 
