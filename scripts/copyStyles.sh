09919d07d3aaef7d926561477ce574b08bfb87cd
Patryk Eliasz <patryk0493@gmail.com>
2020-05-28 11:46:27 +0200

    ci update

--- scripts/copyStyles.sh
+++ scripts/copyStyles.sh
@@ -1,1 +1,1 @@
-cd src && find . -type f \( -iname \*.css -o -iname \*.scss \) -exec cp --parents "{}" ../dest \;
+cd src && find . -type f \( -iname \*.css -o -iname \*.scss \) -exec cp --parents "{}" ../dist \;
