0a8b8a700bf6d7d914be421f17849137cdb32b92
Patryk Eliasz <patryk0493@gmail.com>
2020-06-02 16:19:02 +0200

    samples updates and small fixes

--- src/cell-templates/rateCell/RateCellTemplate.tsx
+++ src/cell-templates/rateCell/RateCellTemplate.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CellTemplate, Cell, Compatible, getCellProperty, Uncertain,UncertainCompatible } from '@silevis/reactgrid';
+import { CellTemplate, Cell, Compatible, getCellProperty, Uncertain, UncertainCompatible } from '@silevis/reactgrid';
 import './rate-cell-style.scss';
 
 export interface RateCell extends Cell {
@@ -15,15 +15,15 @@ export class RateCellTemplate implements CellTemplate<RateCell> {
   getCompatibleCell(uncertainCell: Uncertain<RateCell>): Compatible<RateCell> {
     const value = getCellProperty(uncertainCell, 'value', 'number');
     const text = value.toString();
-    return { ...uncertainCell, value, text};
-}
+    return { ...uncertainCell, value, text };
+  }
 
   textToCellData(cellvalue: number): number {
     if (isNaN(cellvalue) || cellvalue < this.MIN_VAL)
       return this.MIN_VAL
     else if (cellvalue > this.STARS)
       return this.STARS
-    else 
+    else
       return cellvalue
   }
 
@@ -38,16 +38,21 @@ export class RateCellTemplate implements CellTemplate<RateCell> {
   render(cell: Compatible<RateCell>, isInEditMode: boolean, onCellChanged: (cell: Compatible<RateCell>, commit: boolean) => void): React.ReactNode {
     let stars: any[] = [];
     const randNumber = Math.floor(Math.random() * 100000); // TODO get unique ID in grid
-    for(let i = 1; i <= this.STARS; i++) {
+    for (let i = 1; i <= this.STARS; i++) {
       stars.push(
         <React.Fragment key={i}>
-          <input type="radio" id={`star_${i}_input_${randNumber}`} name={`rate_${randNumber}`} value={i} 
-            checked={this.textToCellData(cell.value) === i} onChange={()=> null}
+          <input type="radio" id={`star_${i}_input_${randNumber}`} name={`rate_${randNumber}`} value={i}
+            checked={this.textToCellData(cell.value) === i} onChange={() => null}
           />
-          <label htmlFor={`star_${i}_input_${randNumber}`} title="text" onClick={(e) => { onCellChanged({ ...cell, value: i}, true)}}/>
+          <label htmlFor={`star_${i}_input_${randNumber}`} title="text"
+            onClick={e => {
+              e.preventDefault();
+              onCellChanged({ ...cell, value: i }, true);
+            }}
+          />
         </React.Fragment>
       )
     }
     return <div className="rate">{stars.reverse()}</div>
-  } 
+  }
 }
