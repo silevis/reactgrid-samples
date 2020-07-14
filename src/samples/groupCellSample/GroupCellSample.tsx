037c7fbd86ce4cd4bd89660c1f2c2abf70bd24a1
Patryk Eliasz <patryk0493@gmail.com>
2020-07-14 13:46:25 +0200

    moving group of rows

--- src/samples/groupCellSample/GroupCellSample.tsx
+++ src/samples/groupCellSample/GroupCellSample.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { columns as dataColumns } from '../../data/group/columns';
 import { rows as dataRows, headerRow } from '../../data/group/rows';
-import { Cell, CellChange, Column, GroupCell, ReactGrid, Row, DefaultCellTypes } from '@silevis/reactgrid';
+import { CellChange, Column, GroupCell, ReactGrid, Row, DefaultCellTypes, Id, DropPosition } from '@silevis/reactgrid';
 import './styling.scss';
 
 const ReactGridContainer = styled.div`
@@ -52,17 +52,15 @@ export const GroupCellSample: React.FunctionComponent = () => {
 
     const getDataFromRows = (rows: Row[]): Row[] => rows.filter(row => row.cells.find(cell => cell.type === 'group') !== undefined);
 
-    const createIndents = (rows: Row[]): Row[] => {
-        return rows.map(row => {
-            const groupCell = getGroupCell(row);
-            if (groupCell.parentId === undefined) {
-                const hasRowChildrens = hasChildren(rows, row);
-                groupCell.hasChildren = hasRowChildrens;
-                if (hasRowChildrens) assignIndentAndHasChildrens(rows, row, 0);
-            }
-            return row;
-        });
-    };
+    const createIndents = (rows: Row[]): Row[] => rows.map(row => {
+        const groupCell = getGroupCell(row);
+        if (groupCell.parentId === undefined) {
+            const hasRowChildrens = hasChildren(rows, row);
+            groupCell.hasChildren = hasRowChildrens;
+            if (hasRowChildrens) assignIndentAndHasChildrens(rows, row, 0);
+        }
+        return row;
+    });
 
     const [state, setState] = useState<GroupTestGridStateData>(() => {
         const columns = dataColumns(true, false);
@@ -72,7 +70,7 @@ export const GroupCellSample: React.FunctionComponent = () => {
         return { columns, rows }
     });
 
-    const [rowsToRender, setRowsToRender] = useState<Row[]>([headerRow, ...state.rows]);
+    const [rowsToRender, setRowsToRender] = useState<Row[]>([headerRow, ...getExpandedRows(state.rows)]);
 
     const handleChanges = (changes: CellChange[]) => {
         const newState = { ...state };
@@ -85,12 +83,52 @@ export const GroupCellSample: React.FunctionComponent = () => {
         setRowsToRender([headerRow, ...getExpandedRows(newState.rows)]);
     };
 
+    const reorderArray = <T extends {}>(arr: T[], idxs: number[], to: number) => {
+        const movedElements: T[] = arr.filter((_: T, idx: number) => idxs.includes(idx));
+        to = Math.min(...idxs) < to ? to += 1 : to -= idxs.filter(idx => idx < to).length;
+        const leftSide: T[] = arr.filter((_: T, idx: number) => idx < to && !idxs.includes(idx));
+        const rightSide: T[] = arr.filter((_: T, idx: number) => idx >= to && !idxs.includes(idx));
+        return [...leftSide, ...movedElements, ...rightSide];
+    }
+
+    const handleRowsReorder = (targetRowId: Id, rowIds: Id[], dropPosition: DropPosition) => {
+        const newState = { ...state };
+        const to = state.rows.findIndex(row => row.rowId === targetRowId);
+        let rowIdxs = rowIds.map(id => state.rows.findIndex(r => r.rowId === id));
+
+        if (rowIdxs.length === 1) {
+            const row = state.rows[rowIdxs[0]];
+            rowIdxs = [row, ...new Set(getRowChildren(state.rows, [], row))].map(item => state.rows.findIndex(r => r.rowId === item.rowId));
+        }
+
+        const reorderedRows = reorderArray(state.rows, rowIdxs, to);
+
+        setState({ ...newState, rows: reorderedRows });
+        setRowsToRender([headerRow, ...getExpandedRows(reorderedRows)]);
+    }
+
+    const getRowChildren = (rows: Row[], acc: Row[], row: Row) => {
+        const rowsChildren = getDirectChildrenRows(rows, row);
+        if (!rowsChildren) return [];
+
+        rowsChildren.forEach(childRow => {
+            acc = [...acc, ...getRowChildren(rows, rowsChildren, childRow)];
+        });
+        return acc;
+    }
+
+    const handleCanReorderRows = (targetColumnId: Id, rowIds: Id[], dropPosition: DropPosition): boolean => {
+        return true;
+    }
+
     return (
         <ReactGridContainer id="group-cell-sample">
             <ReactGrid
                 rows={rowsToRender}
                 columns={state.columns}
                 onCellsChanged={handleChanges}
+                onRowsReordered={handleRowsReorder}
+                canReorderRows={handleCanReorderRows}
                 stickyLeftColumns={1}
                 stickyTopRows={1}
                 enableRowSelection
