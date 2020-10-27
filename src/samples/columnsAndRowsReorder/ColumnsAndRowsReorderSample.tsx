8a43954f3c9198eb4b4f345d6898157bda494e4d
Patryk Eliasz <patryk0493@gmail.com>
2020-10-27 08:29:30 +0100

    finished column reorder draft

--- src/samples/columnsAndRowsReorder/ColumnsAndRowsReorderSample.tsx
+++ src/samples/columnsAndRowsReorder/ColumnsAndRowsReorderSample.tsx
@@ -15,50 +15,46 @@ const getPeople = (): Person[] => [
     { name: "", surname: "" }
 ];
 
-const columnIds = [
-    'name',
-    'surname',
-] as const;
-
-type ColumnId = typeof columnIds[number];
-
 declare module "@silevis/reactgrid" {
     interface Column {
         columnId: ColumnId;
     }
 }
+interface ColumnMap {
+    name: 'Name';
+    surname: 'Surname';
+}
+
+const columnMap: ColumnMap = {
+    name: 'Name',
+    surname: 'Surname'
+};
+
+type ColumnId = keyof ColumnMap;
 
 const getColumns = (): Column[] => [
-    { columnId: columnIds[0], width: 150, reorderable: true },
-    { columnId: columnIds[1], width: 200, reorderable: true }
+    { columnId: 'name', width: 150, reorderable: true },
+    { columnId: 'surname', width: 200, reorderable: true }
 ];
 
-// const headerRow = (): Row => {
-//     return {
-//         rowId: "header",
-//         cells: [
-//             { type: "header", text: "Name" },
-//             { type: "header", text: "Surname" }
-//         ]
-//     }
-// };
-
-const getRows = (people: Person[], columnsOrder: ColumnId[]): Row[] => [
-    {
-        rowId: "header",
-        cells: [
-            { type: "header", text: "Name" },
-            { type: "header", text: "Surname" }
-        ]
-    },
-    ...people.map<Row>((person, idx) => ({
-        rowId: idx,
-        cells: [
-            { type: "text", text: person[columnsOrder[0]] },
-            { type: "text", text: person[columnsOrder[1]] }
-        ]
-    }))
-];
+const getRows = (people: Person[], columnsOrder: ColumnId[]): Row[] => {
+    return [
+        {
+            rowId: "header",
+            cells: [
+                { type: "header", text: columnMap[columnsOrder[0]] },
+                { type: "header", text: columnMap[columnsOrder[1]] }
+            ]
+        },
+        ...people.map<Row>((person, idx) => ({
+            rowId: idx,
+            cells: [
+                { type: "text", text: person[columnsOrder[0]] },
+                { type: "text", text: person[columnsOrder[1]] }
+            ]
+        }))
+    ]
+};
 
 const reorderArray = <T extends {}>(arr: T[], idxs: number[], to: number) => {
     const movedElements: T[] = arr.filter((_: T, idx: number) => idxs.includes(idx));
@@ -72,7 +68,7 @@ export const ColumnsAndRowsReorderSample = () => {
     const [people] = React.useState<Person[]>(getPeople());
     const [columns, setColumns] = React.useState<Column[]>(getColumns());
 
-    const rows = getRows(people, columns.map<ColumnId>(c => c.columnId));
+    const rows = getRows(people, columns.map(c => c.columnId));
 
     const handleColumnsReorder = (targetColumnId: Id, columnIds: Id[], dropPosition: DropPosition) => {
         const to = columns.findIndex((column) => column.columnId === targetColumnId);
